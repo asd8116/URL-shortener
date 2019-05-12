57e5bca87106d86ecb352f02c5979c3bae27a883
wanaka <asd8116@gmail.com>
2019-05-12 14:20:33 +0800

    Create files and basic code

---
+++ config/auth.js
@@ -0,0 +1,9 @@
+module.exports = {
+  authenticated: (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    req.flash('warning_msg', '請先登入才能使用')
+    res.redirect('/users/login')
+  }
+}
---
+++ routes/users.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+const passport = require('passport')
+const bcrypt = require('bcryptjs')
+const User = require('../models/user')
+
+// login page
+
+// login
+
+// register page
+
+// register
+
+// logout
+
+module.exports = router
