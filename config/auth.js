2aa79d0f9c689570c7f969b1b7ef0b371f285204
wanaka <asd8116@gmail.com>
2019-05-20 14:49:12 +0800

    Remove needless files

--- config/auth.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  authenticated: (req, res, next) => {
-    if (req.isAuthenticated()) {
-      return next()
-    }
-    req.flash('warning_msg', '請先登入才能使用')
-    res.redirect('/users/login')
-  }
-}
--- routes/auths.js
+++
@@ -1,5 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const passport = require('passport')
-
-module.exports = router
